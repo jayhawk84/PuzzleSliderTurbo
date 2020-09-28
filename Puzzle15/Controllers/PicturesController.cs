505af32bcf9b65fb009e1546c2c470608accb822
Jason Horvath <47703915+jayhawk84@users.noreply.github.com>
2020-09-27 22:47:55 -0700

    SQL Success
    Successfully integrated SQL database with the project, scaffolded the controller, and can now perform CRUD operations inside the app!

---
+++ Puzzle15/Controllers/PicturesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Puzzle15.Models;
+
+namespace Puzzle15.Controllers
+{
+    public class PicturesController : Controller
+    {
+        private readonly Puzzle15Context _context;
+
+        public PicturesController(Puzzle15Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Pictures
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pictures.ToListAsync());
+        }
+
+        // GET: Pictures/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pictures = await _context.Pictures
+                .FirstOrDefaultAsync(m => m.PicId == id);
+            if (pictures == null)
+            {
+                return NotFound();
+            }
+
+            return View(pictures);
+        }
+
+        // GET: Pictures/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pictures/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PicId,PicName")] Pictures pictures)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pictures);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pictures);
+        }
+
+        // GET: Pictures/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pictures = await _context.Pictures.FindAsync(id);
+            if (pictures == null)
+            {
+                return NotFound();
+            }
+            return View(pictures);
+        }
+
+        // POST: Pictures/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PicId,PicName")] Pictures pictures)
+        {
+            if (id != pictures.PicId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pictures);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PicturesExists(pictures.PicId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pictures);
+        }
+
+        // GET: Pictures/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pictures = await _context.Pictures
+                .FirstOrDefaultAsync(m => m.PicId == id);
+            if (pictures == null)
+            {
+                return NotFound();
+            }
+
+            return View(pictures);
+        }
+
+        // POST: Pictures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pictures = await _context.Pictures.FindAsync(id);
+            _context.Pictures.Remove(pictures);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PicturesExists(int id)
+        {
+            return _context.Pictures.Any(e => e.PicId == id);
+        }
+    }
+}
